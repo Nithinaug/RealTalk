5b1ecba72fd399530afd0322cb28e86972c65f17
Nithinaug <nithinaug@gmail.com>
2026-02-21 15:50:44 +0530

    Updated for render deployment

---
+++ backend/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+type ChatMessage struct {
+	Type  string   `json:"type"`
+	User  string   `json:"user"`
+	Text  string   `json:"text"`
+	Users []string `json:"users"`
+}
+
+type ChatClient struct {
+	connection *websocket.Conn
+	username   string
+	clientID   string
+}
+
+var (
+	activeClients = make(map[*ChatClient]bool)
+	clientsLock   sync.Mutex
+
+	wsUpgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ws", handleWebSocket)
+	router.Static("/static", "../web")
+	router.GET("/", serveIndexPage)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server running on port", port)
+	router.Run(":" + port)
+}
+
+func serveIndexPage(c *gin.Context) {
+	c.File("../web/index.html")
+}
+
+func handleWebSocket(c *gin.Context) {
+	wsConn, err := wsUpgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+
+	client := &ChatClient{
+		connection: wsConn,
+		clientID:   uuid.NewString(),
+	}
+
+	clientsLock.Lock()
+	activeClients[client] = true
+	clientsLock.Unlock()
+
+	sendActiveUsers()
+
+	defer func() {
+		clientsLock.Lock()
+		delete(activeClients, client)
+		clientsLock.Unlock()
+		sendActiveUsers()
+		wsConn.Close()
+	}()
+
+	for {
+		var incomingMsg ChatMessage
+		if wsConn.ReadJSON(&incomingMsg) != nil {
+			break
+		}
+
+		switch incomingMsg.Type {
+		case "join":
+			client.username = incomingMsg.User
+			sendActiveUsers()
+
+		case "message":
+			broadcastMessage(incomingMsg)
+		}
+	}
+}
+
+func broadcastMessage(msg ChatMessage) {
+	clientsLock.Lock()
+	defer clientsLock.Unlock()
+
+	for client := range activeClients {
+		client.connection.WriteJSON(msg)
+	}
+}
+
+func sendActiveUsers() {
+	var usernames []string
+
+	clientsLock.Lock()
+	for client := range activeClients {
+		if client.username != "" {
+			usernames = append(usernames, client.username)
+		}
+	}
+	clientsLock.Unlock()
+
+	broadcastMessage(ChatMessage{
+		Type:  "users",
+		Users: usernames,
+	})
+}
