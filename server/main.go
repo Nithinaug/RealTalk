23b755e33df16138ffaca052d86f064e50360bc1
Nithinaug <nithin@example.com>
2026-03-15 15:12:07 +0530

    Updated readme

--- server/main.go
+++ server/main.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 	"strings"
-	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
@@ -215,10 +215,6 @@ func findPath(target string) string {
 }
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
